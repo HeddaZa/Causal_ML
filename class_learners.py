d426825166260a3146db0864b41d9571b003724e
Hedda G <hedda.gressel@gmail.com>
2022-07-04 14:27:27 +0200

    corrST

--- class_learners.py
+++ class_learners.py
@@ -53,44 +53,22 @@ class SLearner:
         test_split=0.5,
         random_state=42
     ):
-        self.treatment_dummy = pd.get_dummies(treatment)
-        features_and_treatment = pd.concat([features, self.treatment_dummy],axis =1)
+        treatment = pd.DataFrame(treatment).astype('category')
+        dummy_treatment = pd.get_dummies(treatment)
+        self.dummy_name = dummy_treatment.columns
+        features_and_treatment = pd.concat([features, dummy_treatment],axis =1)
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             features_and_treatment,
             target,
             test_size=test_split,
             random_state=random_state
             )
-        self.X_per_segment = None
+        self.X_test_per_segment = None 
         self.proba_per_segment = None
-
-# def __init__(
-#         self,
-#         features, 
-#         treatment,
-#         target,
-#         test_split=0.5,
-#         random_state=42
-#     ):
-#         self.number_of_unique_treatments = treatment.nunique()
-#         treatment = pd.DataFrame(treatment).astype('category')
-#         dummy_treatment = pd.get_dummies(treatment)
-#         features_and_treatment = pd.concat([features,dummy_treatment], axis = 1)
-#         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-#             features_and_treatment,
-#             target,
-#             test_size=test_split,
-#             random_state=random_state
-#             )
-#         self.X_per_segment = None
-#         self.y_per_segment = None
-#         self.X_test_per_segment = None
-#         self.dummy_name = dummy_treatment.columns
     
     
     def s_learner_segment(self, data, option):
-        cols = self.treatment_dummy.columns
-        for i, col in enumerate(cols):
+        for i, col in enumerate(self.dummy_name):
             data[col] = 0
             if i == option:
                 data[col] = 1
@@ -98,25 +76,23 @@ class SLearner:
 
 
     def _prepare_segments_test(self):
-        self.X_per_segment = {}
-        keys_segment = ["segment_0_1", "segment_1_1", "segment_2_1"]
-        for j in range(3):
-            self.X_per_segment[keys_segment[j]] = self.s_learner_segment(
+        self.X_test_per_segment = {}
+        for j, name in enumerate(self.dummy_name):
+            self.X_test_per_segment[name] = self.s_learner_segment(
                 self.X_test, j
             ).copy()
 
     def _run_predictions(self, classifier, **kwrds):
         self.proba_per_segment = {}
-        keys_segment = ["segment_0_1", "segment_1_1", "segment_2_1"]
         clf = classifier(**kwrds)
         if classifier == xgb.XGBClassifier:
             eval_set = [(self.X_train,self.y_train),(self.X_test,self.y_test)]
             clf.fit(self.X_train,self.y_train, eval_set = eval_set, verbose  = False)
         else:
             clf.fit(self.X_train, self.y_train)
-        for j in range(3):
-            self.proba_per_segment[keys_segment[j]] = clf.predict_proba(
-                self.X_per_segment[keys_segment[j]]
+        for  name in self.dummy_name:
+            self.proba_per_segment[name] = clf.predict_proba(
+                self.X_test_per_segment[name]
             )[:,1]
 
     def get_proba(self,classifier, **kwrds):
