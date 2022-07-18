ffcb19bf441c426bc920dd90cb52ce4bd3e7f9c6
Hedda G <hedda.gressel@gmail.com>
2022-07-18 15:11:40 +0200

    README and plots

--- plots.py
+++ plots.py
@@ -1,12 +1,16 @@
-"""Definitions used for plots"""
+"""
+Definitions used for plots
+
+Please note that these definitions contain many hard coded elements and are meant only to be used for this project
+"""
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# plt.style.use('fivethirtyeight')
 
 
 def pie_plots(data, treatment_name, target_name):
+    '''Pie plots for the ratio of successes per treatment'''
     treatments = data[treatment_name].unique()
     explode = (0, 0.1)
     fig, axes = plt.subplots(1, 4, figsize=(14, 7))
@@ -38,16 +42,19 @@ def pie_plots(data, treatment_name, target_name):
 
 
 def _cat_gender(data):
+    '''map of the two columns "mens" and "womens" to a column "gender" with 0, 1 for women, men, respectively'''
     data["gender"] = 1
     data.loc[data["mens"] == 1, "gender"] = 2
     return data
 
 def map_visit(series):
+    '''maps 0, 1 to "no visit", "visit", respectively'''
     map_visit = {0:'no visit', 1: 'visit'}
     return series.map(map_visit)
 
 
 def plot_categorical(data):
+    '''plot categorical features split by visit/no visit'''
     data = _cat_gender(data)
     data['visit'] = map_visit(data['visit'])
     custom_params = {"axes.spines.right": False, "axes.spines.top": False}
@@ -90,6 +97,7 @@ def plot_categorical(data):
 
 
 def plot_continuous(data):
+    '''plots continuous features split by visit/no visit'''
     x = data[data["visit"] == 'no visit']["recency"]
     y = data[data["visit"] == 'visit']["recency"]
 
@@ -125,7 +133,11 @@ def plot_continuous(data):
 
 
 def plot_ERUPT(S_dict, T_dict, cST_dict):
-
+    '''
+    plots the ERUPT metric value of the classifier, 
+    a boxplot with the random ERUPT values if the outcomes of the classifier were shuffles, 
+    and the mean of all visits
+    '''
     fig, ax = plt.subplots(1, 3, sharey=True, figsize=(15, 7))
     fig.set_facecolor("snow")
 
@@ -182,4 +194,52 @@ def plot_ERUPT(S_dict, T_dict, cST_dict):
         0.07, 0.5, "ERUPT values", va="center", rotation="vertical", fontsize="x-large"
     )
 
-    plt.plot()
+    plt.show()
+
+def pie_plot_best_outcome(data,lst, name_all):
+    '''pie plots of model suggestions'''
+    explode = (0.1, 0.1,0.1)
+    fig, axes = plt.subplots(1, len(lst), figsize=(14, 7))
+    fig.set_facecolor("snow")
+    df = data.apply(lambda x: x.value_counts()).fillna(0)
+    labels = ['no email', "women's email","men's email"]
+    titles = ['Logistic Regression', 'Random Forest','XGBoost']
+    if df.shape[0] != 3:
+        missing_indeces = set([0,1,2]) - set(df.index)
+        for missing_index in missing_indeces:
+            df.loc[missing_index] = 0
+    for i,name in enumerate(lst):
+        patches, _, _ = axes[i].pie(
+            df[name].sort_index(),
+            colors=["thistle", "lightblue",'seagreen'],
+            shadow=True,
+            explode=explode,
+            autopct="%1.0f%%"
+        )
+        axes[i].set_title(titles[i])
+    axes[len(lst)-1].legend(patches, labels,bbox_to_anchor=(1.05, 0.65))
+    fig.suptitle(name_all, y=0.83, fontweight = 'bold', size = 'x-large')
+
+def pie_plot_xgb(data, name_all):
+    '''pie plot of corrST_Learner suggestions'''
+    explode = (0.1, 0.1,0.1)
+    fig, axes = plt.subplots( figsize=(9, 6))
+    fig.set_facecolor("snow")
+    df = data.apply(lambda x: x.value_counts()).fillna(0)
+    labels = ['no email', "women's email","men's email"]
+    titles = ['XGBoost']
+    if df.shape[0] != 3:
+        missing_indeces = set([0,1,2]) - set(df.index)
+        for missing_index in missing_indeces:
+            df.loc[missing_index] = 0
+
+    patches, _, _ = axes.pie(
+        df['xgb'].sort_index(),
+        colors=["thistle", "lightblue",'seagreen'],
+        shadow=True,
+        explode=explode,
+        autopct="%1.0f%%"
+    )
+    axes.set_title(titles[0])
+    axes.legend(patches, labels,bbox_to_anchor=(1.05, 0.65))
+    fig.suptitle(name_all, y=0.98, fontweight = 'bold', size = 'x-large')
